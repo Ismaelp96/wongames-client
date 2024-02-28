76d790c2a8d14deecf8e27bf270231952dcb562f
ismaelp96 <ismael6991@live.com>
2024-02-28 13:50:52 -0300

    Criando um helper de testes renderWithTheme

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,9 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the a white label by default', () => {
+    // renderizar o compoenente `render`
+    // selecionar o elemento a ser testado `screen` (queries) - getByLabel ...
+    // expect - assertion - asserção - comparação - analise
   })
 })
