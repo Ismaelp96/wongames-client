495494df0fdc424d8309d40228c24a02a8e7fa5e
ismaelp96 <ismael6991@live.com>
2024-02-28 14:34:18 -0300

    Criando primeiros testes

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import { renderWithTheme } from '../../utils/tests/helpers'
 
 describe('<Logo />', () => {
   it('should render the a white label by default', () => {
     // renderizar o compoenente `render`
     // selecionar o elemento a ser testado `screen` (queries) - getByLabel ...
     // expect - assertion - asserção - comparação - analise
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render the a black labelwhen color is passed', () => {
+    // renderizar o compoenente `render`
+    // selecionar o elemento a ser testado `screen` (queries) - getByLabel ...
+    // expect - assertion - asserção - comparação - analise
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
